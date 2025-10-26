5b6b4afcd8ab83fadaa6fd6c1b453d1986b654ae
hboline <hnboline@gmail.com>
2025-10-26 13:40:48 -0500

    expanded pluralizer functionality

--- app/proofreading/basic_string_manipulation.py
+++ app/proofreading/basic_string_manipulation.py
@@ -27,14 +27,26 @@ def lower(word: str) -> str:
     word = word.lower()
     return word
 
+VOWELS = "aeiou"
 def pluralize(word: str) -> str:
-    if word[-1] == 's':
-        return word[:-1]
+    """Return pluralized form of word based on general heuristic rules.
+    Cannot go from plural to singular, and will not work on some words.\n
+    e.g. \"cand[y]\" -> \"cand[ies]\""""
+    last_1 = word[-1]
+    last_2 = word[-2:]
+    if last_2 in ['ss','ch','sh'] or last_1 in ['s','x','z']:
+        return word + 'es'
+    elif last_2[0] not in VOWELS and last_1 == 'y':
+        return word[:-1] + 'ies'
+    elif ((val := last_2) == 'fe') or ((val := last_1) == 'f'):
+        return word[:-len(val)] + 'ves'
     else:
         return word + 's'
 
-VOWELS = "aeiou"
 def past_tensifier_simple(word: str) -> str:
+    """Return past-tense of present-tense word based on general heuristic rules.
+    Cannot go from past-tense to present-tense.\n
+    e.g. \"pani[c]\" -> \"pani[cked]\""""
     if word[-1] == 'e':
         return word + 'd'
     elif word[-1] == 'y' and word[-2] not in VOWELS:
