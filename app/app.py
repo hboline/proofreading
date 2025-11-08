bd057a3b291e72a9374bd9c66c1645a067f1cd7c
hboline <hnboline@gmail.com>
2025-11-08 16:06:22 -0600

    cleaned up & commented process_function

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, astuple
 from typing import Callable, List, Optional
 import inspect
 
@@ -60,76 +60,93 @@ class App():
                 self.state.error = e
 
     def process_action(self, FC: FuncContainer):
-        func = FC.func
-        func_type = FC.func_type
-        paste_type = FC.paste_type
-        is_special = FC.special
-        special_default = FC.special_default
-
-        args = list()
-        kwargs = dict()
+        # explicit definition of FuncContainer types
+        func: Callable
+        func_type: FuncType
+        paste_type: PasteOption
+        is_special: bool
+        special_default: Optional[str]
         
+        # get all fields of FuncContainer as tuple
+        func,\
+        func_type,\
+        paste_type,\
+        is_special,\
+        special_default\
+        = astuple(FC)
+        
+        # get reader window, clipboard object, and save clipboard
         reader = self.state.reader_window
         c = self.state.clipboard
+        c.save()
        
+        # NOTE: this is only used for special functions and for printing function to history
+        args = list()
+        
+        # get additional input if function is special (consider baking this into FuncType somehow)
         if is_special is True:
             input = curses.keyname(self.state.screen.getch()).decode()
             if input == special_default:
                 args.append(None)
             else:
                 args.append(input)
-        
-        if func_type is FuncType.Super:
-            func(self)
-            return
-        
-        reader.activate()
-        c.copy()
-        c.save()
        
-        # process input based on function type
+        # perform actions based on FuncType
         word: str = ''
+        result: Optional[str] = None
         match func_type:
             case FuncType.Default:
+                reader.activate()
+                c.copy()
                 word = c.get()
             case FuncType.NoCopy:
                 assert isinstance(func, partial)
                 word = func.args[0]
                 func = func.func
+            case FuncType.Super:
+                result = func(self) # NOTE: might need a better way to handle this
+                if result is None:
+                    return
         
+        # get any additional args/kwargs
+        # NOTE: this currently only looks for a State object from the app and nothing else
+        kwargs = dict()
         func_kwargs = {k:str(v.annotation) for k,v in inspect.signature(func).parameters.items()}
         for k,v in func_kwargs.items():
             if v == 'State':
                 kwargs[k] = self.state
         
-        # process action function
+        # call function, get output (if any), and catch exceptions
         try:
-            result = func(word, *args, **kwargs)
+            if func_type is not FuncType.Super:
+                result = func(word, *args, **kwargs)
+            assert result is not None
         except AssertionError:
-            c.reset()
-            raise Exception("input is not string")
+            raise Exception("input is not a string")
         except Exception as e:
-            c.reset()
             raise e
-        else:
-            args.insert(0, word)
-            
-            self.state.action_history.insert(0, 
-                f"{function_stringifier(func, *args)}" + \
-                (f" -> \"{result}\"" if result and self.state.vars.show_output else '')
-            )
-            
-            # process function output based on paste type
-            match paste_type:
-                case PasteOption.Bracketed:
-                    c.set(f"[{result}]")
-                case PasteOption.Raw:
-                    c.set(result)
-                case PasteOption.Nothing:
-                    c.reset()
-                    return
-            c.paste()
+        finally:
             c.reset()
+
+        # get string representation of function for printing function to history in main UI
+        args.insert(0, word)
+        self.state.action_history.insert(0, 
+            f"{function_stringifier(func, *args)}" + \
+            (f" -> \"{result}\"" if result and self.state.vars.show_output else '')
+        )
+        
+        # switch to reader window and paste based on PasteOption
+        if not reader.is_active():
+            reader.activate()
+        match paste_type:
+            case PasteOption.Nothing:
+                return
+            case PasteOption.Bracketed:
+                c.set(f"[{result}]")
+            case PasteOption.Raw:
+                c.set(result)
+        c.paste()
+        c.reset()
         
     def handle_result(self, result: UIResult):
         # check next UI
